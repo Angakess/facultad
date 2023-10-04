aca184863672263c813d93f3a168ad844b0ad9a3
Andres <andresgabriel.kessler@gmail.com>
2023-10-04 17:06:32 -0300

    muchas cosas

--- 3.2/ISO/Practica 3/probandoBash.sh
+++ 3.2/ISO/Practica 3/probandoBash.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-cadena1="hola"
-cadena2="mundo"
-concatenada=$(expr "$cadena1" : '\(.*\)'"$cadena2")
-echo $concatenada
+
+arreglo=($*)
+
+echo "Longitud: ${#arreglo[@]}"
+echo "Todos los elementos: ${arreglo[@]}"
+aux=${arreglo[1]}
+arreglo[1]=${arreglo[3]}
+arreglo[3]=$aux
+echo "Elementos revertidos: ${arreglo[@]}"
 
