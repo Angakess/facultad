d1c231242a1733f36c287e0da33428c194860792
Andres Kessler <andresgabriel.kessler@gmail.com>
2024-04-12 13:44:12 -0300

    cosas

---
+++ 4.1/OO2/ejercicio1/src/main/java/ar/edu/unlp/info/oo2/ejercicio1/Main.java
@@ -0,0 +1,18 @@
+package ar.edu.unlp.info.oo2.ejercicio1;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int x = 10;
+		while (x > 5) {
+			System.out.printf("el valor de x es: ", x);
+			x--; 
+		}
+		System.out.printf("fin del loop");
+	}
+
+}
