6947a089d867c3ab85aec176311bfe47f7672705
Andres Kessler <andresgabriel.kessler@gmail.com>
2024-04-23 18:38:09 -0300

    practica de cplp

---
+++ 4.1/OO2/asdf/src/asdf/cuerpo.java
@@ -0,0 +1,14 @@
+package asdf;
+
+public class cuerpo {
+
+	public cuerpo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
